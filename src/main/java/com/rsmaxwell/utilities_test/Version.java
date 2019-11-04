94e0dcb6fe08a0af39c009193fa7f6a1548cb301
Richard Maxwell <richard@rsmaxwell.co.uk>
2019-11-04 08:32:42 +0000

    update

---
+++ src/main/java/com/rsmaxwell/utilities_test/Version.java
@@ -0,0 +1,34 @@
+package com.rsmaxwell.utilities_test;
+
+public class Version {
+
+	// buildID function
+	public static String version() {		
+		return "<UTILITIES_TEST_VERSION>";
+	}
+
+	// buildDate function
+	public static String buildDate() {
+		return "<BUILD_DATE>";
+	}
+
+	// gitCommit function
+	public static String  gitCommit() {
+		return "<GIT_COMMIT>";
+	}
+
+	// gitBranch function
+	public static String  gitBranch() {
+		return "<GIT_BRANCH>";
+	}
+
+	// gitURL function
+	public static String  gitURL() {
+		return "<GIT_URL>";
+	}
+	
+	// utilities_version function
+	public static String utilities_version() {		
+		return "<UTILITIES_VERSION>";
+	}
+}
